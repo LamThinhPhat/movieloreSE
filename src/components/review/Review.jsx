bc5dd89b0a79e541525c8fb37d90ca21def8503e
LamThinhPhat <92035772+LamThinhPhat@users.noreply.github.com>
2021-12-28 08:52:38 +0700

    add filter bar

--- src/components/review/Review.jsx
+++ src/components/review/Review.jsx
@@ -1,21 +1,33 @@
 import Post from "../post/Post";
-import "./review.scss"
+import "./review.scss";
+import Filters from '../filters/Filters'
 
-
-function Review() {
+function Review(props) {
     return (
         <div className="review-container">
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-
-            <Post />
-            <Post />
-            <Post />
-            <Post />
+            <Filters />
+            <div className="post-list">
+                {props.filter && props.filter.length > 0 && props.filter.map(item => {
+                    return (
+                        <Post key={item.id}
+                            id={item.id}
+                            athor={item.athor}
+                            cast={item.cast}
+                            director={item.director}
+                            gerne={item.gerne.join(', ')}
+                            name={item.name}
+                            plot={item.plot}
+                            poster={item.poster}
+                            rate={item.rate}
+                            releaseDate={item.releaseDate}
+                            review={item.review}
+                            time={item.time}
+                        />
+                    )
+                })}
+            </div>
         </div>
+
     )
 }
 
